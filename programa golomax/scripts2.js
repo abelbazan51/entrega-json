d2b02c38ba483a2720fb247e37a5ad27f9375dc8
abel bazan <abelbazan51@gmail.com>
2022-06-21 20:38:43 -0300

    entregafinalterminada

--- programa golomax/scripts2.js
+++ programa golomax/scripts2.js
@@ -1,7 +1,21 @@
-let array = [];
+const Clickbotonindex = document.querySelectorAll(".botonindex");
 
-fetch("./datos.json|")
-  .then((resp) => resp.JSON())
-  .then((data) => {
-    array = data;
-  });
+Clickbotonindex.forEach((botonindex) => {
+  btnalerta.addEventListener("click", addTodatosindex);
+});
+
+function addTodatosindex(e) {
+  const boton = e.target;
+  const nombrecliente = item.querySelector(".nombre").textcontent;
+  const apellidocliente = item.querySelector(".datos").textcontent;
+  const docimicilio = item.querySelector(".direccion").textcontent;
+  const datosadicionales = item.querySelector(".datosadicionales").textcontent;
+
+  const clientes = {
+    nombre: nombrecliente,
+    apellido: apellidocliente,
+    docimicilio: docimicilio,
+    datos: datosadicionales,
+  };
+  console.log(clientes);
+}
